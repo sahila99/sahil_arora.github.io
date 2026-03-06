6971e8e7dd07e659718768d53ff4c0ea4a12824f
Sahil Arora <sahil@Sahils-MacBook-Pro.local>
2026-03-06 16:46:40 +0530

    Add OG image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
-// @ts-check
 import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://sahila99.github.io",
-  output: "static",
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  site: "https://sahilarora.tech",
+  integrations: [sitemap()],
 });
